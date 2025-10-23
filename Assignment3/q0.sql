5dbcba96d086b4e38f108a80151bec69836981ec
Manleyb <brycemanleyy@gmail.com>
2025-10-23 15:34:14 -0700

    updating q0

--- Assignment3/q0.sql
+++ Assignment3/q0.sql
@@ -6,11 +6,11 @@ SELECT
     e.last_name        -- Manager's last name
 FROM dept_emp de     -- 'dm' is an alias for the dept_manager table
 JOIN employees e 
-ON de.emp_no = e.emp_no         -- Match each manager with their employee record
-   -- Match each manager's department number with its name
-WHERE de.to_date = '9999-01-01' -- Only include current (active) managers
+ON de.emp_no = e.emp_no         
+  
+WHERE de.to_date = '9999-01-01' -
 
-ORDER BY e.birth_date ASC     -- Sort results by salary in descending order (highest first)
+ORDER BY e.birth_date ASC    
 LIMIT 1;
 
 
